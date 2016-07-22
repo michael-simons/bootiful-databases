bcbd0bf8fe5214d6a9aaa822f6d7597be290c035
Michael J. Simons <michael@simons.ac>
2016-07-22 18:35:00 +0200

    Goto start.spring.io and generate project
    In the first step, we go to http://start.spring.io and generate a project containing
    
    * web
    * jOOQ
    * H2
    
    this will get us so called startes for all those libraries. Those starters provide automatic configuration for their libraries. We then have an embedded container (Tomcat), working Spring MVC and an embedded H2 database, so that this project actually compiles and runs without errors.
    
    I also prefer to have the Spring Boot devtools on the classpath, so that I don't have to restart the app during development.

---
+++ src/main/java/ac/simons/doag2016/Application.java
@@ -0,0 +1,12 @@
+package ac.simons.doag2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
