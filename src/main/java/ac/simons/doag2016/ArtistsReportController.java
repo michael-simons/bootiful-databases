16b0f225a35df885a4a587703293417be40fb197
Michael J. Simons <michael@simons.ac>
2016-07-27 12:32:41 +0200

    Create a more advanced query
    You know see how even arrays of stuff can be mapped to query parameters, in this case an array of ids.
    
    This is then used as a query parameter.
    
    The window function now needs a partition, in this case: the artist.

--- src/main/java/ac/simons/doag2016/ArtistsReportController.java
+++ src/main/java/ac/simons/doag2016/ArtistsReportController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.jooq.DSLContext;
 import org.jooq.DatePart;
 import org.jooq.Field;
+import org.jooq.impl.DSL;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import static ac.simons.doag2016.db.Tables.PLAYS;
 import static ac.simons.doag2016.db.Tables.TRACKS;
 import static org.jooq.impl.DSL.count;
 import static org.jooq.impl.DSL.orderBy;
+import static org.jooq.impl.DSL.partitionBy;
 import static org.jooq.impl.DSL.sum;
 import static org.jooq.impl.DSL.trunc;
 
@@ -41,9 +43,9 @@ public class ArtistsReportController {
                 .formatJSON(response.getOutputStream());
     }
 
-    @RequestMapping(path = "/{artistId}/cumulativePlays")
+    @RequestMapping(path = "/{artistIds}/cumulativePlays")
     public void getCumulativePlays(
-            @PathVariable final BigDecimal artistId,
+            @PathVariable final BigDecimal[] artistIds,
             final HttpServletResponse response
     ) throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
@@ -51,12 +53,12 @@ public class ArtistsReportController {
         this.create
                 .select(playedOnTruncatedToDay,
                         ARTISTS.ARTIST,
-                        sum(count()).over(orderBy(playedOnTruncatedToDay)).as("cumulativePlays")
+                        sum(count()).over(partitionBy(ARTISTS.ARTIST).orderBy(playedOnTruncatedToDay)).as("cumulativePlays")
                 )
                 .from(PLAYS)
                 .join(TRACKS).onKey()
                 .join(ARTISTS).onKey()
-                .where(ARTISTS.ID.eq(artistId))
+                .where(ARTISTS.ID.in(artistIds))
                 .groupBy(playedOnTruncatedToDay,
                         ARTISTS.ARTIST
                 )
