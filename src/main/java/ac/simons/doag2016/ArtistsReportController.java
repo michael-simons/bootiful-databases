de26eb211da3580ae4c03f90a2bb938603a2b10a
Michael J. Simons <michael@simons.ac>
2016-07-27 11:43:38 +0200

    Introduce a more interesting query
    This is the kind of query you won’t write down using any ORM like EclipseLink or Hibernate.
    
    It isn’t even very compilicated: It uses an analytic function to compute the running total or cumulative playcount for one given artist over all plays.
    
    Regarding Spring / Spring Boot: It’s a fine example how URL parameters can be mapped to function parameter.

--- src/main/java/ac/simons/doag2016/ArtistsReportController.java
+++ src/main/java/ac/simons/doag2016/ArtistsReportController.java
@@ -1,13 +1,24 @@
 package ac.simons.doag2016;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Date;
 import javax.servlet.http.HttpServletResponse;
 import org.jooq.DSLContext;
+import org.jooq.DatePart;
+import org.jooq.Field;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static ac.simons.doag2016.db.Tables.ARTISTS;
+import static ac.simons.doag2016.db.Tables.PLAYS;
+import static ac.simons.doag2016.db.Tables.TRACKS;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.orderBy;
+import static org.jooq.impl.DSL.sum;
+import static org.jooq.impl.DSL.trunc;
 
 @Controller
 @RequestMapping("/api/artists")
@@ -27,6 +38,32 @@ public class ArtistsReportController {
                 .from(ARTISTS)
                 .orderBy(ARTISTS.ARTIST.asc())
                 .fetch()
-                .formatJSON(response.getOutputStream());        
+                .formatJSON(response.getOutputStream());
+    }
+
+    @RequestMapping(path = "/{artistId}/cumulativePlays")
+    public void getCumulativePlays(
+            @PathVariable final BigDecimal artistId,
+            final HttpServletResponse response
+    ) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+        final Field<Date> playedOnTruncatedToDay = trunc(PLAYS.PLAYED_ON, DatePart.DAY).cast(Date.class);
+        this.create
+                .select(playedOnTruncatedToDay,
+                        ARTISTS.ARTIST,
+                        sum(count()).over(orderBy(playedOnTruncatedToDay)).as("cumulativePlays")
+                )
+                .from(PLAYS)
+                .join(TRACKS).onKey()
+                .join(ARTISTS).onKey()
+                .where(ARTISTS.ID.eq(artistId))
+                .groupBy(playedOnTruncatedToDay,
+                        ARTISTS.ARTIST
+                )
+                .orderBy(playedOnTruncatedToDay,
+                        ARTISTS.ARTIST
+                )
+                .fetch()
+                .formatJSON(response.getOutputStream());
     }
 }
