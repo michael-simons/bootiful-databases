8283cdc485cdcba285aba8e673479ebe00aff581
Michael J. Simons <michael@simons.ac>
2016-07-27 15:29:17 +0200

    Restrict query by an optional date range
    Shows the usage of @RequestParams, including support for Java 8 features (Optional and new java.time.*) types in Spring.
    
    Shows mapping from an Optional to a condition.

--- src/main/java/ac/simons/doag2016/ArtistsReportController.java
+++ src/main/java/ac/simons/doag2016/ArtistsReportController.java
@@ -3,21 +3,25 @@ package ac.simons.doag2016;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import org.jooq.DSLContext;
 import org.jooq.DatePart;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static ac.simons.doag2016.db.Tables.ARTISTS;
 import static ac.simons.doag2016.db.Tables.PLAYS;
 import static ac.simons.doag2016.db.Tables.TRACKS;
 import static org.jooq.impl.DSL.count;
-import static org.jooq.impl.DSL.orderBy;
 import static org.jooq.impl.DSL.partitionBy;
 import static org.jooq.impl.DSL.sum;
 import static org.jooq.impl.DSL.trunc;
@@ -46,6 +50,12 @@ public class ArtistsReportController {
     @RequestMapping(path = "/{artistIds}/cumulativePlays")
     public void getCumulativePlays(
             @PathVariable final BigDecimal[] artistIds,
+            @RequestParam
+            @DateTimeFormat(iso = ISO.DATE)
+            final Optional<LocalDate> from,
+            @RequestParam
+            @DateTimeFormat(iso = ISO.DATE)
+            final Optional<LocalDate> to,
             final HttpServletResponse response
     ) throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
@@ -59,6 +69,14 @@ public class ArtistsReportController {
                 .join(TRACKS).onKey()
                 .join(ARTISTS).onKey()
                 .where(ARTISTS.ID.in(artistIds))
+                .and(from.map(Date::valueOf)
+                        .map(playedOnTruncatedToDay::greaterOrEqual)
+                        .orElseGet(DSL::trueCondition)
+                )
+                .and(to.map(Date::valueOf)
+                        .map(playedOnTruncatedToDay::lessOrEqual)
+                        .orElseGet(DSL::trueCondition)
+                )
                 .groupBy(playedOnTruncatedToDay,
                         ARTISTS.ARTIST
                 )
