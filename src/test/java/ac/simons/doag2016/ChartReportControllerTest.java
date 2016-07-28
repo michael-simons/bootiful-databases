1cef80638becc65988c75cba33a5edb81f7c59e2
Michael J. Simons <michael@simons.ac>
2016-07-28 15:27:29 +0200

    Add an example on how controllers directly depending on jOOQ can be tested
    This is a nice example on why I prefer to abstract the DSLContext inside an @Repository or something similar. Such an @Repository could be mocked away with @MockBean and I could use @WebMvcTest for example.
    
    It would be possible to use if I create different configuration classes for the test, but that would be a little too much now for the demo.

---
+++ src/test/java/ac/simons/doag2016/ChartReportControllerTest.java
@@ -0,0 +1,69 @@
+package ac.simons.doag2016;
+
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.jooq.tools.jdbc.MockConnection;
+import org.jooq.tools.jdbc.MockDataProvider;
+import org.jooq.tools.jdbc.MockResult;
+import org.jooq.util.oracle.OracleDataType;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ChartReportControllerTest {
+
+    @Test
+    public void getChartsShouldWork() throws Exception {
+
+        // Create a Mock data provider for jOOQ
+        final DSLContext create = DSL.using(SQLDialect.ORACLE);
+        final MockDataProvider mockDataProvider = ctx -> {
+            final MockResult[] result = new MockResult[1];
+            result[0] = new MockResult(0, create.newResult(
+                    DSL.field("label", OracleDataType.VARCHAR2),
+                    DSL.field("cnt", OracleDataType.INTEGER),
+                    DSL.field("change", OracleDataType.INTEGER)
+            ));
+            return result;
+        };
+        // The mock connectiong that is created with the above data provider
+        final MockConnection connection = new MockConnection(mockDataProvider);
+
+        // Instantiate the report controller.
+        // This is not my favorite way of doing this.
+        // I'd favor the @WebMvcTest annotation on tests, but using that
+        // it get's kinda hard to mock the DSLContext, so I skipped that for this
+        // example.
+        final ChartReportController controller = new ChartReportController(DSL.using(connection, create.dialect()));
+
+        final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/api/charts/{year}/{month}", 2016, 11))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json("{\n"
+                        + "  \"fields\": [\n"
+                        + "    {\n"
+                        + "      \"name\": \"label\",\n"
+                        + "      \"type\": \"VARCHAR\"\n"
+                        + "    },\n"
+                        + "    {\n"
+                        + "      \"table\": \"previous_month\",\n"
+                        + "      \"name\": \"cnt\",\n"
+                        + "      \"type\": \"INTEGER\"\n"
+                        + "    },\n"
+                        + "    {\n"
+                        + "      \"name\": \"change\",\n"
+                        + "      \"type\": \"INTEGER\"\n"
+                        + "    }\n"
+                        + "  ], "
+                        + "\"records\": []"
+                        + "}"));
+
+    }
+}
