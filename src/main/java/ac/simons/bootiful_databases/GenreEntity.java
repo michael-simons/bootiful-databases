2d8d8297426269ac841d8352ddeb08e6dea1f4b5
Michael J. Simons <michael@simons.ac>
2017-05-09 13:39:45 +0200

    Add GenreEntity

---
+++ src/main/java/ac/simons/bootiful_databases/GenreEntity.java
@@ -0,0 +1,39 @@
+package ac.simons.bootiful_databases;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "genres")
+public class GenreEntity implements Serializable {
+
+    private static final long serialVersionUID = 2970999815981518236L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "genre")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
